fbbe238553083165f43eda704f56f4a5a11e31f1
duvaudierje <jerome.duvaudier@natixis.com>
2021-09-10 16:50:24 +0200

    init project

---
+++ src/main/java/com/wcs/citimmo/CitimmoApplication.java
@@ -0,0 +1,13 @@
+package com.wcs.citimmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitimmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitimmoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wcs/citimmo/CitimmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcs.citimmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitimmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
