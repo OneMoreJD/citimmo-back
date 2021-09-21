293e3a845e2d08f1df1d888dcb74b6d6a644b6ac
Maxime Caspani <maxime.caspani@natixis.com>
2021-09-21 14:37:47 +0200

    Added script to init the DB

---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE citibase;
+CREATE USER 'citiboss'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'c1t1b0$$';
+GRANT ALL ON citibase.* TO  'citiboss'@'localhost';
+FLUSH PRIVILEGES ;
